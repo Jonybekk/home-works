20b6c179a79a41e89ff456d274fa38afdd6ee715
Jonybekk <joniarmanuli@icloud.com>
2023-11-15 21:09:27 +0600

    Update hw-3.py

--- python/hw-15.11/hw-3.py
+++ python/hw-15.11/hw-3.py
@@ -23,4 +23,20 @@ print(student)
 #4
 print(len(student))
 #5
-print(student ["skills"])
+print(student ["skills"])print(student ["skills"])
+# 6
+student["skills"].append("Lie down")
+print(student)
+student_keys = student.keys()
+print(student_keys)
+# 8
+student_value = student.values()
+print(student_value)
+# 9
+student_lists = student.items()
+print(student_lists)
+# 10
+print(student.pop("city"))
+# 11
+del student
+print(student)
