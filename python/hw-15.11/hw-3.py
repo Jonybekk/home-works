2f140fcfbd4818f1f8f8a624ac21299432070abe
Jonybekk <joniarmanuli@icloud.com>
2023-11-15 17:12:44 +0600

    hw

---
+++ python/hw-15.11/hw-3.py
@@ -0,0 +1,26 @@
+#Home work
+#1
+dog = {}
+#2
+dog = {
+    "name": "Valy",
+    "age": 4,
+    "color": "brown",
+    "breed": "Akita"
+}
+#3
+student = {
+    "name":"Era",
+    "lname":"Esteuov",
+    "floor":"man",
+    "age": 15,
+    "country":"Kazakhstan",
+    "marital_status":"good",
+    "city":"Almaty",
+    "skills": ["eat food", "reading book", "to run", "sit"]
+}
+print(student)
+#4
+print(len(student))
+#5
+print(student ["skills"])
